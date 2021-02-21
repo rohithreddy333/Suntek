e85aecd48e247d023e1d7386b96e7e865b9e1314
rohithreddy333 <rohithreddydhoodhipala333@gmail.com>
2021-02-21 17:52:52 +0530

    added comments

--- 21 feb/digitFrequency.java
+++ 21 feb/digitFrequency.java
@@ -6,12 +6,22 @@ public class digitFrequency
 		Scanner s  = new Scanner(System.in);
 		System.out.print("enter string : ");
 		String str = s.nextLine();
+	
+		//hashmap to store digits as keys and store the repeating values
+
 		HashMap<Character,Integer> digitCounter = new HashMap<Character,Integer>();
 		
+		// Initialising the repeating values to 0 
 		for(int i=48; i < 58; i++){
 		    digitCounter.put((char)i,0);
 		}
+
+		//converting string to character array
+
 		char [] c = str.toCharArray();
+
+		//appending 1 to value whenever key repeats
+
 		for(int i =0; i < c.length ; i++){
 		    if(digitCounter.containsKey(c[i])){
 		        digitCounter.put(c[i] , digitCounter.get(c[i]) + 1);
