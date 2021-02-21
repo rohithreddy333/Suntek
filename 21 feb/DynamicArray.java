73779bee4fe76edc125ca244aea4cb6269e6594d
Rohith Reddy D <50194466+rohithreddy333@users.noreply.github.com>
2021-02-21 22:10:18 +0530

    added comments

--- 21 feb/DynamicArray.java
+++ 21 feb/DynamicArray.java
@@ -5,18 +5,25 @@ public class DynamicArray
 	public static void main(String[] args) {
 		Scanner s  = new Scanner(System.in);
 		
+		// input of number of shelves
 		int shelves = s.nextInt();
+		
+		//creating and initialising array of arraylist for pages( substitute for pointer to pointer logic from c)
 		
 		ArrayList<Integer> [] pages = new ArrayList[shelves];
 		for(int i=0 ; i < shelves; i++){
 		    pages[i] = new ArrayList<Integer>();
 		}
 		
+		// array for number of books in each shelf and initialising it to zero
+		
 		int [] books = new int [shelves];
 		for(int i = 0; i < shelves; i++){
 		    books[i] = 0;
 		}
 		int queries = s.nextInt();
+		
+		//iterating queries
 		while(queries > 0){
 		    int queryType = s.nextInt();
 		    
